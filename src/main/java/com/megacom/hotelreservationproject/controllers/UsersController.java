f904bc441167b33fd0188662dd14bc3334a3bb30
Arthur Jakypbek <arthurjkk43@gmail.com>
2022-06-06 19:04:49 +0600

    Add dao & controller

---
+++ src/main/java/com/megacom/hotelreservationproject/controllers/UsersController.java
@@ -0,0 +1,9 @@
+package com.megacom.hotelreservationproject.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UsersController {
+}
